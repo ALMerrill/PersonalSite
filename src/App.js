c59540f6135c371477350437ec0970a034b45e6d
Andrew <merrillman32@gmail.com>
2019-07-22 21:01:13 -1000

    transfer to react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Andrew</h1>9{" "}
+        <p>
+          Hi - this is the start of my personal site. It is automatically
+          deployed through AWS S3 each time I commit new code to GitHub.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
