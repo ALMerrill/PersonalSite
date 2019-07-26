5e419f4aeb8a0b172a3fc2143df125eddb7f4d27
Andrew <merrillman32@gmail.com>
2019-07-25 17:53:13 -1000

    updated information and title of app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ function App() {
         <h1>Andrew</h1>
         <p>
           Hi - this is the start of my personal site. It is automatically
-          deployed through AWS S3 each time I commit new code to GitHub.
+          deployed to an AWS S3 bucket each time I commit new code to GitHub.
+          This deployment if performed by a webhook to AWS CodePipeline that
+          grabs the updated code, builds it using CodeBuild, and deploys it with
+          an automatic AWS cli command. I'll be adding more information about
+          myself now that I have most of the pipeline set up.
         </p>
       </header>
     </div>
